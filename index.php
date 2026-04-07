428a08bb850c0ca60104046c3ecf85535e145c48
gonzalezalan-23 <facundoalangonzalez08@gmail.com>
2026-04-06 21:00:18 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio - Alan González</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/footer.css">
+    <link rel="stylesheet" href="assets/css/header.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/section-about-me.css">
+    <link rel="stylesheet" href="assets/css/section-experiences.css">
+    <link rel="stylesheet" href="assets/css/section-projects.css">
+    <link rel="stylesheet" href="assets/css/section-services.css">
+    <link rel="stylesheet" href="assets/css/section-skills.css">
+    <link rel="stylesheet" href="assets/css/section-video-presentation.css">
+    <base href="assets/">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <?php
+        include "includes/header.php";
+        include "includes/main.php";
+        include "includes/section-about-me.php";
+        include "includes/section-services.php";
+        include "includes/section-skills.php";
+        include "includes/section-experiences.php";
+        include "includes/section-projects.php";
+        include "includes/section-video-presentation.php";
+        include "includes/footer.php";
+    ?>
+    <script src="js/section-about-me.js"></script>
+    <script src="js/section-experiences.js"></script>
+    <script src="js/section-projects.js"></script>
+    <script src="js/section-skills.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <script src="js/header.js"></script>
+</body>
+</html>
